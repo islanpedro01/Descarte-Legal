ac8c5a46b5314b81f95a9d08c560f1c2b4e852ac
Islan Pedro <113214808+islanpedro01@users.noreply.github.com>
2023-05-02 00:07:45 -0300

    Add files via upload

---
+++ js/cards.js
@@ -0,0 +1,19 @@
+const cards =[
+{
+    img:'./imgs/informe-se.png',
+    title:'Informe-se para uma cidade mais verde!',
+    content:'Aqui nós lhe damos dicas de boas práticas para tornar sua cidade mais verde! Tornando-a em um lugar melhor para todos!'
+},
+{    
+    img:'./imgs/gps.png',
+    title:'Descubra os locais ideais para o descarte!',
+    content:'Tenha acesso a localização exata dos locais mais indicados para o despejo dos seus resíduos!'
+},
+{
+    img:'./imgs/denuncie.png',
+    title:'Ajude a fiscalização e denuncie!',
+    content:'Trabalhe com o poder público auxiliando na identificação dos responsáveis pelo descarte irregular!'
+}
+];
+
+export default cards;
